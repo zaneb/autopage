37045d06008baca2065c307efd6e9c6f1dbe631f
Zane Bitter <zbitter@redhat.com>
2021-04-01 16:47:30 -0400

    Add line_buffer_from_input() function
    Add a function to determine whether line buffering should be enabled
    when the data being written to the console is being ingested from a
    given input stream.

--- autopage/__init__.py
+++ autopage/__init__.py
@@ -27,7 +27,7 @@ import typing
 from typing import Any, Optional, Type, TextIO
 
 
-__all__ = ['AutoPager']
+__all__ = ['AutoPager', 'line_buffer_from_input']
 
 
 class AutoPager:
@@ -129,3 +129,27 @@ class AutoPager:
             if isinstance(exc, OSError):
                 return True
         return False
+
+
+def line_buffer_from_input(input_stream: Optional[typing.IO] = None) -> bool:
+    """
+    Return whether line buffering should be enabled for a given input stream.
+
+    When data is being read from an input stream, processed somehow, and then
+    written to an autopaged output stream, it may be desirable to enable line
+    buffering on the output. This means that each line of data written to the
+    output will be visible immediately, as opposed to waiting for the output
+    buffer to fill up. This is, however, slower.
+
+    If the input stream is a file, line buffering is unnecessary. This function
+    determines whether an input stream might require line buffering on output.
+
+    If no input stream is specified, sys.stdin is assumed.
+
+        >>> with AutoPager(line_buffering=line_buffer_from_input()) as out:
+        >>>     for l in sys.stdin:
+        >>>         out.write(l)
+    """
+    if input_stream is None:
+        input_stream = sys.stdin
+    return not input_stream.seekable()
