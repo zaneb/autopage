bd782c139f6b6205a387bcd6c296bf5fe19aceaa
Zane Bitter <zbitter@redhat.com>
2021-04-01 13:00:44 -0400

    Add setuptools packaging

---
+++ setup.py
@@ -0,0 +1,25 @@
+[metadata]
+name = autopage
+author = Zane Bitter
+author_email = zbitter@redhat.com
+description = A library to provide automatic paging for console output
+url = https://github.com/zaneb/autopage
+classifiers =
+    License :: OSI Approved
+    License :: OSI Approved :: Apache Software License
+    Programming Language :: Python :: 3
+    Programming Language :: Python :: 3 :: Only
+    Programming Language :: Python :: 3.6
+    Programming Language :: Python :: 3.7
+    Programming Language :: Python :: 3.8
+    Operating System :: POSIX
+    Environment :: Console
+    Intended Audience :: Developers
+    Topic :: Software Development :: Libraries
+    Topic :: Software Development :: Libraries :: Python Modules
+    Topic :: Terminals
+    Topic :: Utilities
+
+[options]
+packages = find:
+python_requires = >=3.6
