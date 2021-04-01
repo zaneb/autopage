5e5abc28582f62c5341227153feefa3954d5d893
Zane Bitter <zbitter@redhat.com>
2021-04-01 12:33:10 -0400

    Convert to Context Manager class
    Using the contextlib.contextmanager decorator is becoming unwieldy.

--- autopage/__init__.py
+++ autopage/__init__.py
@@ -18,7 +18,6 @@ A library to provide automatic paging for console output.
 By Zane Bitter.
 """
 
-import contextlib
 import io
 import subprocess
 import sys
@@ -27,65 +26,79 @@ import sys
 __all__ = ['AutoPager']
 
 
-@contextlib.contextmanager
-def AutoPager(output_stream=None, line_buffer=False):
+class AutoPager:
     """
     A context manager that launches a pager for the output if appropriate.
 
     If the output stream is not to the console (i.e. it is piped or
     redirected), no pager will be launched.
     """
-    use_stdout = output_stream is None
-    if use_stdout:
-        output_stream = sys.stdout
 
-    if not output_stream.isatty():
-        if line_buffer:
+    def __init__(self, output_stream=None, line_buffering=False):
+        self._use_stdout = output_stream is None or output_stream is sys.stdout
+        self._out = sys.stdout if output_stream is None else output_stream
+        self._line_buffering = line_buffering
+        self._pager = None
+
+    def __enter__(self):
+        # Only invoke the pager if the output is going to a tty; if it is
+        # being sent to a file or pipe then we don't want the pager involved
+        if self._out.isatty():
+            return self._paged_stream()
+        else:
+            self._reconfigure_output_stream()
+            return self._out
+
+    def _reconfigure_output_stream(self):
+        if self._line_buffering:
             # Python 3.7 & later
-            if hasattr(output_stream, 'reconfigure'):
-                output_stream.reconfigure(line_buffering=line_buffer)
+            if hasattr(self._out, 'reconfigure'):
+                self._out.reconfigure(line_buffering=self._line_buffering)
             else:
                 # Pure-python I/O
-                if hasattr(output_stream, '_line_buffering'):
-                    output_stream._line_buffering = line_buffer
-                    output_stream.flush()
+                if hasattr(self._out, '_line_buffering'):
+                    self._out._line_buffering = self._line_buffering
+                    self._out.flush()
                 # Native I/O on Python 3.6
-                elif (isinstance(output_stream, io.TextIOWrapper) and
-                        not output_stream.line_buffering):
+                elif (isinstance(self._out, io.TextIOWrapper) and
+                        not self._out.line_buffering):
                     args = {
-                        'encoding': output_stream.encoding,
-                        'errors': output_stream.errors,
+                        'encoding': self._out.encoding,
+                        'errors': self._out.errors,
+                        'line_buffering': self._line_buffering,
                     }
-                    if use_stdout:
+                    if self._use_stdout:
                         sys.stdout = None
-                    newstream = io.TextIOWrapper(output_stream.detach(),
-                                                 line_buffering=line_buffer,
-                                                 **args)
-                    output_stream = newstream
-                    if use_stdout:
+                    newstream = io.TextIOWrapper(self._out.detach(), **args)
+                    self._out = newstream
+                    if self._use_stdout:
                         sys.stdout = newstream
-        yield output_stream
-        return
 
-    streams = {} if use_stdout else {'stdout': output_stream}
-    streams['stdin'] = subprocess.PIPE
-    args = ['--RAW-CONTROL-CHARS']  # Enable colour output
-    if not line_buffer:
-        args.append('--quit-if-one-screen')
-    pager = subprocess.Popen(['less'] + args,
-                             bufsize=1 if line_buffer else -1,
-                             errors='backslashreplace',
-                             **streams)
-    try:
-        with contextlib.closing(pager.stdin) as stream:
-            yield stream
-    except OSError:
-        pass
-    finally:
-        while True:
-            try:
-                pager.wait()
-                break
-            except KeyboardInterrupt:
-                # Pager ignores Ctrl-C, so we should too
-                pass
+    def _paged_stream(self):
+        streams = {} if self._use_stdout else {'stdout': self._out}
+        streams['stdin'] = subprocess.PIPE
+        args = ['--RAW-CONTROL-CHARS']  # Enable colour output
+        if not self._line_buffering:
+            args.append('--quit-if-one-screen')
+        buffer_size = 1 if self._line_buffering else -1
+        self._pager = subprocess.Popen(['less'] + args,
+                                       bufsize=buffer_size,
+                                       errors='backslashreplace',
+                                       **streams)
+        return self._pager.stdin
+
+    def __exit__(self, exc_type, exc, traceback):
+        if self._pager is not None:
+            while True:
+                try:
+                    self._pager.wait()
+                except KeyboardInterrupt:
+                    # Pager ignores Ctrl-C, so we should too
+                    continue
+                else:
+                    break
+
+        if exc is not None:
+            if isinstance(exc, OSError):
+                return True
+        return False
