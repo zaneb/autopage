9c7db3657104e65a3b6963f57cad52a4c6285735
Zane Bitter <zbitter@redhat.com>
2021-04-23 17:11:01 -0400

    unit tests: Test context manager enter

--- autopage/tests/test_autopage.py
+++ autopage/tests/test_autopage.py
@@ -134,6 +134,27 @@ class ToTerminalTest(unittest.TestCase):
                 ap = autopage.AutoPager()
             self.assertFalse(ap.to_terminal())
 
+    def test_launch_pager(self):
+        ap = autopage.AutoPager()
+        with mock.patch.object(ap, 'to_terminal', return_value=True), \
+                mock.patch.object(ap, '_paged_stream') as page, \
+                mock.patch.object(ap, '_reconfigure_output_stream') as reconf:
+            with ap as stream:
+                page.assert_called_once()
+                self.assertIs(page.return_value, stream)
+                reconf.assert_not_called()
+
+    def test_no_pager(self):
+        outstream = mock.Mock()
+        ap = autopage.AutoPager(outstream)
+        with mock.patch.object(ap, 'to_terminal', return_value=False), \
+                mock.patch.object(ap, '_paged_stream') as page, \
+                mock.patch.object(ap, '_reconfigure_output_stream') as reconf:
+            with ap as stream:
+                page.assert_not_called()
+                self.assertIs(outstream, stream)
+                reconf.assert_called_once()
+
 
 class ExitCodeTest(fixtures.TestWithFixtures):
     def setUp(self):
