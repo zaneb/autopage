39b8627ccc120fcd5955e914dd737f1263cfd141
Zane Bitter <zbitter@redhat.com>
2023-05-24 23:28:12 +1200

    integration tests: Make timeouts more robust
    Ensure that if the test process does not exit, we'll still open the
    FIFOs in the "shell" process so that the parent process does not block
    forever.

--- autopage/tests/isolation.py
+++ autopage/tests/isolation.py
@@ -190,6 +190,11 @@ class TestProcessNotComplete(Exception):
     pass
 
 
+class TimeoutException(Exception):
+    def __init__(self, pid: Optional[int]):
+        super(Exception, self).__init__(f'Test process {pid} timed out')
+
+
 @contextlib.contextmanager
 def isolate(child_function: typing.Callable[[], int],
             stdin_pipe: bool = False,
@@ -226,14 +231,18 @@ def isolate(child_function: typing.Callable[[], int],
                 signal.signal(signal.SIGINT, signal.SIG_IGN)
 
                 p.join(2)  # Wait at most 2s
+                if p.exitcode is None:
+                    raise TimeoutException(p.pid)
             except BaseException:
-                with os.fdopen(result_w, 'w') as result_writer:
-                    traceback.print_exc(file=result_writer)
-                # Prevent blocking in parent process by opening our end of all
-                # FIFOs.
-                for path, write in zip(fifo_paths, [False, True, True]):
-                    _open_fifo(path, write)
-                os._exit(1)
+                try:
+                    with os.fdopen(result_w, 'w') as result_writer:
+                        traceback.print_exc(file=result_writer)
+                finally:
+                    # Prevent blocking in parent process by opening our end of
+                    # all FIFOs.
+                    for path, write in zip(fifo_paths, [False, True, True]):
+                        _open_fifo(path, write)
+                    os._exit(1)
             with os.fdopen(result_w, 'w') as result_writer:
                 result_writer.write(f'{p.exitcode}\n')
 
